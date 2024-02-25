4792393c1aa29943127ee7ecb25cb1a4c09c8fb7
VADDIJASWANT <116190897+VADDIJASWANT@users.noreply.github.com>
2024-02-25 20:47:12 +0530

    modifications with skill cards
    Added border radius
    add max width for mobile view
    add bg color and hover effect for certifiates part in skill card

--- src/components/skillCard.jsx
+++ src/components/skillCard.jsx
@@ -7,14 +7,14 @@ export default function skillCard(props){
     let certificates = props.additionalContents.certificates
     console.log(certificates)
     const certificatesHTML = certificates.map((certificate,index) => {
-        return(<p>
+        return(<p className='bg-lightWhite hover:text-white hover:bg-blue-400 p-2 rounded-lg' key={index}>
             <a href={certificate.Url} target='_blank' rel="noreferrer">{certificate.Name}</a>
         </p>)
     })
     console.log(certificatesHTML)
     return(
         <>
-            <div className="popup p-5 font-kalam flex flex-col w-full gap-5 bg-white min-w-[18.75rem] md:max-w-xl">
+            <div className="popup p-5 font-kalam rounded-lg flex flex-col w-full gap-5 bg-white min-w-[18.75rem] max-w-[80%] md:max-w-xl">
                 <div className='flex gap-5 w-full items-center'>
                     <img className="object-contain w-1/4 h-8 md:h-16" src={props.Logo} alt={props.Name}/>
                     <h3 className="text-2xl">{props.Name}</h3>
